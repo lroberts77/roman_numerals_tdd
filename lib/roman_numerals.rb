e651f51183e8046ef9addd3ddbe89b958ae805ef
liam roberts <liamroberts77@googlemail.com>
2020-04-25 14:35:26 +0100

    added hash, empty roman_numeral string and calculation for conversion

--- lib/roman_numerals.rb
+++ lib/roman_numerals.rb
@@ -1,11 +1,16 @@
 class Roman_numerals
+    ROMAN_NUMERALS = {
+        1 => "I", 4 => "IV", 5 => "V", 9 => "IX", 10 => "X", 40 => "XL", 50 => "L",
+        90 => "XC", 100 => "C", 400 => "CD", 500 => "D", 900 => "CM", 1000 => "M"
+    }
     def self.convert(num)
-        if num == 2 
-            return "II"
+        roman_numeral = ""
+        ROMAN_NUMERALS.reverse_each do |k, v|
+            (num / k).times { roman_numeral << v; num -= k}
         end
-        if num == 5 
-            return "V"
-        end
-        "I"
+        roman_numeral
     end
 end
+        
+        
+        
--- spec/roman_numerals_spec.rb
+++ spec/roman_numerals_spec.rb
@@ -15,6 +15,27 @@ describe Roman_numerals do
         it "should convert 5 to V" do
             expect(Roman_numerals.convert(5)).to eq ("V")
         end
+
+        it "should convert 10 to X" do
+            expect(Roman_numerals.convert(10)).to eq ("X")
+        end
+
+        it "should convert 35 to XXXV" do
+            expect(Roman_numerals.convert(35)).to eq("XXXV")
+        end
+
+        it "should convert 469 to CDLXIX" do
+            expect(Roman_numerals.convert(469)).to eq("CDLXIX")
+        end
+
+        it "should convert 999 to CMXCIX" do
+            expect(Roman_numerals.convert(999)).to eq("CMXCIX")
+        end
+
+        it "should convert 1948 to MCMXLVIII" do
+            expect(Roman_numerals.convert(1948)).to eq("MCMXLVIII")
+        end
+        
     end
 
 
